593c1f13a9e0cba140c2564bb26200c0f38e8c14
Nauder <prestes.nauder@gmail.com>
2025-04-16 22:20:08 -0300

    fix: update PyInstaller command to use absolute path for UnityPy data

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ pyinstaller `
     --onefile `
     --noconsole `
     --icon "./qtdesigner/images/icon.ico" `
-    --add-data ".venv/Lib/site-packages/UnityPy/;UnityPy/" `
+    --add-data "$env:LOCALAPPDATA\Programs\Python\Python311\Lib\site-packages\UnityPy;UnityPy/" `
     --hidden-import "fastparquet" `
     --name "Floowandereeze & Modding" `
     .\main.py
