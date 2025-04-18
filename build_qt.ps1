0233d5a86df52d7970fe49123f44d51ccfd243c8
Nauder <prestes.nauder@gmail.com>
2025-04-18 17:57:57 -0300

    fix: update build_qt.ps1

--- build_qt.ps1
+++ build_qt.ps1
@@ -8,7 +8,12 @@ $output_dir = ".\pages\ui"
 # Get all .ui files and convert them to Python files
 Get-ChildItem -Path $ui_dir -Filter *.ui | ForEach-Object {
     $inputFile = $_.FullName
-    $outputFile = "$output_dir\$($_.BaseName).py"
+    $baseName = $_.BaseName
+    # Special case for mainwindow.ui to match the import
+    if ($baseName -eq "mainwindow") {
+        $baseName = "main_window"
+    }
+    $outputFile = "$output_dir\$baseName.py"
     pyside6-uic $inputFile -o $outputFile --from-imports
     Write-Output "Converted $inputFile to $outputFile"
 }
